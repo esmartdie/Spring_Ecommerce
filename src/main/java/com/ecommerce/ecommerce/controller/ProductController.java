707a9a63d718a8f62d5aaac4fb9c20de0f7358f4
esmartdie <esmartdie@gmail.com>
2023-11-05 19:20:36 +0100

    develop the save product functionality

--- src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
@@ -1,7 +1,13 @@
 package com.ecommerce.ecommerce.controller;
 
+import com.ecommerce.ecommerce.model.Product;
+import com.ecommerce.ecommerce.model.User;
+import com.ecommerce.ecommerce.service.ProductService;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +16,24 @@ public class ProductController {
 
     @GetMapping("")
     public String show(){
-        return "products/show.html";
+        return "products/show";
     }
+
+    @GetMapping("/create")
+    public String create(){
+        return "products/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product){
+        LOGGER.info("This is the object product {}", product);
+
+        User u = new User(1,"","","","","","","");
+        product.setUser(u);
+        productoService.save(product);
+        return "redirect:/products";
+    }
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+    @Autowired
+    private ProductService productoService;
 }
