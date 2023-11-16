6d52e477b0c4be6efdd5fdcb790bde814014ec2d
esmartdie <esmartdie@gmail.com>
2023-11-16 23:34:05 +0100

    show shopping details on user rol front

--- src/main/java/com/ecommerce/ecommerce/controller/AdministratorController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/AdministratorController.java
@@ -1,13 +1,17 @@
 package com.ecommerce.ecommerce.controller;
 
+import com.ecommerce.ecommerce.model.Order;
 import com.ecommerce.ecommerce.model.Product;
 import com.ecommerce.ecommerce.service.IOrderService;
 import com.ecommerce.ecommerce.service.IProductService;
 import com.ecommerce.ecommerce.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -40,7 +44,20 @@ public class AdministratorController {
         return "administrator/orders";
     }
 
+    @GetMapping("/details/{id}")
+    public String details(Model model, @PathVariable Integer id){
 
+        LOGG.info("Order id: {}", id);
+
+        Order order = orderService.findById(id).get();
+
+        model.addAttribute("orderdetails", order.getDetails());
+
+        return "administrator/shoppingdetails";
+    }
+
+
+    private final Logger LOGG = LoggerFactory.getLogger(AdministratorController.class);
 
     @Autowired
     private IProductService productService;
