7b85579f13c794d307688653e6479ea3c81262c9
esmartdie <esmartdie@gmail.com>
2024-07-15 20:33:39 +0200

    changes before merge

--- src/main/java/com/ecommerce/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/ecommerce/EcommerceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class EcommerceApplication {
 
 	public static void main(String[] args)  throws InterruptedException{
-		// Añadir un retraso de 10 segundos
+
 		System.out.println("Esperando a que la base de datos esté lista...");
 		Thread.sleep(10000);
 		SpringApplication.run(EcommerceApplication.class, args);
